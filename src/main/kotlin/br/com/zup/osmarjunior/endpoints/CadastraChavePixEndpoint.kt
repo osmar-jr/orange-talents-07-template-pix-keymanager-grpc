e0fe8145b95a0c24f30ab45289e5cf256eb401a7
osmar-jr <osmar.junior@zup.com.br>
2021-09-27 09:59:06 -0300

    Registrando uma nova chave Pix v2 Excepton Handler adicionado

--- src/main/kotlin/br/com/zup/osmarjunior/endpoints/CadastraChavePixEndpoint.kt
+++ src/main/kotlin/br/com/zup/osmarjunior/endpoints/CadastraChavePixEndpoint.kt
@@ -5,6 +5,7 @@ import br.com.zup.osmarjunior.ChavePixResponse
 import br.com.zup.osmarjunior.KeyManagerGrpcServiceGrpc
 import br.com.zup.osmarjunior.exceptions.ChavePixExistenteException
 import br.com.zup.osmarjunior.service.NovaChavePixService
+import br.com.zup.osmarjunior.shared.handlers.ErrorAroundHandler
 import br.com.zup.osmarjunior.utils.toModel
 import io.grpc.Status
 import io.grpc.stub.StreamObserver
@@ -12,6 +13,7 @@ import jakarta.inject.Inject
 import jakarta.inject.Singleton
 import javax.validation.ConstraintViolationException
 
+@ErrorAroundHandler
 @Singleton
 class CadastraChavePixEndpoint(
     @Inject val service: NovaChavePixService
@@ -23,48 +25,17 @@ class CadastraChavePixEndpoint(
         responseObserver: StreamObserver<ChavePixResponse>?
     ) {
 
-        try {
-            val novaChavePix = request.toModel()
-            val chavePixCriada = service.registrar(novaChavePix)
+        val novaChavePix = request.toModel()
+        val chavePixCriada = service.registrar(novaChavePix)
 
-            val response = ChavePixResponse.newBuilder()
-                .setClientId(chavePixCriada.identificadorCliente.toString())
-                .setPixId(chavePixCriada.id.toString())
-                .build()
+        val response = ChavePixResponse.newBuilder()
+            .setClientId(chavePixCriada.identificadorCliente.toString())
+            .setPixId(chavePixCriada.id.toString())
+            .build()
 
-            responseObserver?.onNext(response)
-            responseObserver?.onCompleted()
+        responseObserver?.onNext(response)
+        responseObserver?.onCompleted()
 
-        } catch (e: ChavePixExistenteException){
-
-            responseObserver?.onError(
-                Status.ALREADY_EXISTS
-                    .withDescription(e.message)
-                    .withCause(e)
-                    .asRuntimeException()
-            )
-            return
-        } catch (e: ConstraintViolationException){
-
-            responseObserver?.onError(
-                Status.INVALID_ARGUMENT
-                    .withDescription("Erro de Validação.")
-                    .augmentDescription(e.message)
-                    .withCause(e)
-                    .asRuntimeException()
-            )
-            return
-        } catch (e: Exception){
-
-            responseObserver?.onError(
-                Status.INTERNAL
-                    .withDescription("Erro no sistema.")
-                    .augmentDescription(e.message)
-                    .withCause(e)
-                    .asRuntimeException()
-            )
-            return
-        }
 
     }
 }
---
+++ src/main/kotlin/br/com/zup/osmarjunior/shared/handlers/ErrorAroundHandlerInterceptor.kt
@@ -0,0 +1,41 @@
+package br.com.zup.osmarjunior.shared.handlers
+import br.com.zup.osmarjunior.exceptions.ChavePixExistenteException
+import io.grpc.Status
+import io.grpc.stub.StreamObserver
+import io.micronaut.aop.InterceptorBean
+import io.micronaut.aop.MethodInterceptor
+import io.micronaut.aop.MethodInvocationContext
+import jakarta.inject.Singleton
+import javax.validation.ConstraintViolationException
+
+@Singleton
+@InterceptorBean(ErrorAroundHandler::class)
+class ErrorAroundHandlerInterceptor : MethodInterceptor<Any, Any> {
+
+    override fun intercept(context: MethodInvocationContext<Any, Any>): Any? {
+
+        try {
+            return context.proceed()
+        } catch (ex: Exception) {
+
+            val responseObserver = context.parameterValues[1] as StreamObserver<*>
+
+            val status = when(ex) {
+                is ConstraintViolationException -> Status.INVALID_ARGUMENT
+                    .withCause(ex)
+                    .withDescription(ex.message)
+                is ChavePixExistenteException -> Status.ALREADY_EXISTS
+                    .withCause(ex)
+                    .withDescription(ex.message)
+                else -> Status.UNKNOWN
+                    .withCause(ex)
+                    .withDescription("Ops, um erro inesperado ocorreu")
+            }
+
+            responseObserver.onError(status.asRuntimeException())
+        }
+
+        return null
+    }
+
+}
